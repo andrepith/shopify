c14917045f8cdd48994b3bbfa9515e478e2a6302
Andre Adikara <andrepith@yahoo.co.uk>
2021-02-28 20:58:40 +0700

    Setup redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import { store } from "./store";
 import routes from "./routes";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        {routes.map((route, key) => (
-          <Route
-            key={`${route.path} - ${key}`}
-            path={route.path}
-            component={route.component}
-            exact={route.exact}
-          />
-        ))}
-      </Switch>
-    </Router>
+    <div className="container">
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            {routes.map((route, key) => (
+              <Route
+                key={`${route.path} - ${key}`}
+                path={route.path}
+                component={route.component}
+                exact={route.exact}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </Provider>
+    </div>
   );
 }
 
