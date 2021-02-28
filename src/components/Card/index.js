7f9fdf0c2dd3e49562c4cdc0d427646f82f7daab
Andre Adikara <andrepith@yahoo.co.uk>
2021-02-28 20:27:54 +0700

    Create Card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const Card = ({ title, id, addToCart }) => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="card p-4">
+      <div className="flex-grow-1">{title}</div>
+      <div className="d-flex ml-auto">
+        <button
+          className="card card-action"
+          onClick={() => setCount(count ? count - 1 : 0)}
+        >
+          -
+        </button>
+        <div className="card-action">{count}</div>
+        <button
+          className="card card-action"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+      </div>
+      <button
+        className="card mt-2 ml-auto px-4 cursor-pointer"
+        onClick={addToCart(id, count)}
+      >
+        Add to cart
+      </button>
+    </div>
+  );
+};
+
+export default Card;
